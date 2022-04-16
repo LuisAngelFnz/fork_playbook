923b0a034cfc0c984d1f43c51ae318eb1b7dac89
LuisAngelFnz <lfernandez@gmail.com>
2022-04-16 00:18:42 -0500

    Se sube example6

---
+++ weekly_mission_1/example6/main.js
@@ -0,0 +1,6 @@
+require('./patcher') // Llamas este módulo que modifica el objeto instanciado
+const logger = require('./logger') // Al llamar el módulo en logger.js te dará el objeto modificado
+
+
+logger.customMessage();
+logger.log('Other message');
