3e794df08a7ed02ebca75e0541195d61240f2aa3
LuisAngelFnz <lfernandez@gmail.com>
2022-04-16 00:19:37 -0500

    Se sube example8

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,15 @@
+
+class Pokemon {
+  constructor (name){
+    this.name = name;
+  }
+
+  sayHello (message){
+    console.log(`Pokemon ${this.name} say Hellooooo..`);
+  }
+  sayMessage (greeting){
+    console.log(`Mesage from pokemon: ${this.name}::: ${greeting}`);
+  }
+}
+
+module.exports = Pokemon;
