9bf1bf9c73ac92c9f52836c086db0d93ee09e855
LuisAngelFnz <lfernandez@gmail.com>
2022-04-21 01:49:05 -0500

    2 Entregable de objeto con argumentos

--- weekly_mission_2/0_objetos.js
+++ weekly_mission_2/0_objetos.js
@@ -1,4 +1,34 @@
 // Crear un objeto vacío
 const obj_avoid = {}
 
+// Crear un objeto con propiedades
+const obj_properties = {
+    'obj_name'            : 'any',
+    'obj_last_name'       : 'your lastname',
+    'obj_other_propertie' : 'yess'
+}
+
+console.log('Ejecutando objeto con propiedades');
+console.log(obj_properties);
+
+// Crear un objeto con propiedades diferentes
+const obj_some_properties = {
+    'name':'Luis',
+    'last_name':'Fernandez',
+    'hobbies':[
+        'Dance',
+        'Play Futbol',
+    ],
+    'parents':{
+        'Mother':{
+            'name':'Rosalia',
+        },
+        'Father':{
+            'name':'Agustin'
+        }
+    }
+}
+console.log('Ejecutando objeto con propiedades diferentes');
+console.log(obj_some_properties);
+
 
