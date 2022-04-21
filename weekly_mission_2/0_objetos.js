34c44cc1b4c54a11c8c380c228a24f736e4cbf51
LuisAngelFnz <lfernandez@gmail.com>
2022-04-21 01:49:53 -0500

    2 Entregable de objeto con metodos

--- weekly_mission_2/0_objetos.js
+++ weekly_mission_2/0_objetos.js
@@ -31,4 +31,30 @@ const obj_some_properties = {
 console.log('Ejecutando objeto con propiedades diferentes');
 console.log(obj_some_properties);
 
+// Crear un objeto con métodos
+const obj_functions_args_avoid = {
+    'name':{
+        'emmiter'  : 'Luis',
+        'receiver' : 'Angel'
+    },
+    'call':function(){ //Con arrow functions no funciona ;(
+        console.log(this.name);
+        console.log(`Llamada entrante de ${this.name.emmiter} hacia ${this.name.receiver}`);
+    }
+}
 
+console.log('Ejecutando objeto con métodos');
+obj_functions_args_avoid.call()
+
+// Crear un objeto que reciba parámetros
+const obj_functions_args = {
+    'name':{
+        'emmiter':'Luis',
+        'receiver':'Angel'
+    },
+    'call':function(message){
+        console.log(`Llamada entrante de ${this.name.emmiter} hacia ${this.name.receiver}, con el mensaje: ${message}`);
+    }
+}
+console.log('Ejecutando objeto con métodos y argumentos');
+obj_functions_args.call('Son la 1:42 a.m. FFFF')
