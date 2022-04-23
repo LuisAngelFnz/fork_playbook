85a55f5467a29e51d9add77605127ba697e90036
LuisAngelFnz <lfernandez@gmail.com>
2022-04-23 12:18:42 -0500

    3 Agregando resolucion de enunciado 3

--- weekly_mission_2/examples/modelado/example_2.js
+++ weekly_mission_2/examples/modelado/example_2.js
@@ -65,3 +65,15 @@ explorers.forEach(each_explorer => console.log(each_explorer.name));
 console.log('Imprime el stack de cada explorer, usa FOR EACH')
 explorers.forEach(each_explorer => console.log(each_explorer.stack))
 
+console.log('Crea una nueva lista con las listas de stacks de cada explorer, usa MAP')
+console.log(explorers.map(each_explorer => each_explorer.stack));
+
+console.log('Obtén la lista de explorers que tengan en su stack "js", usa FILTER (para validar un elemento en un lista se usa el método includes)');
+
+console.log(explorers.filter(each_explorer => each_explorer.stack.includes('js')))
+
+
+// Busca el primer explorer que sea de la CDMX, usa FIND
+// Obtén la suma de todos los exercises_completed, usa REDUCE
+// Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME
+// Obtén la validación si todos los explorers tienen la propiedad isFinished del onboarding como true. Usa EVERY.
