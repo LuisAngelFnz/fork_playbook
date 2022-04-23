4c3dd9c806c771485a6d2222155a1332cc233d19
LuisAngelFnz <lfernandez@gmail.com>
2022-04-22 20:07:23 -0500

    Agregando script de ejercicio con clases con funciones

---
+++ weekly_mission_2/examples/clases_y_objetos/4_clases_objetos.js
@@ -0,0 +1,24 @@
+// Ejemplo 4: Métodos en los objetos
+class Repository {
+  constructor(name, author, language, stars){
+   this.name = name
+   this.author = author
+   this.language = language
+   this.stars = stars
+  }
+
+  getInfo(){ // es una función que ejecutará cualquier objeto instanciado de esta clase
+    return `Repository ${this.name} has ${this.stars} stars`
+  }
+}
+
+console.log("Ejemplo 4: Métodos en los objetos")
+
+const myRepo = new Repository("LaunchX", "carlogilmar", "js", 100)
+
+console.log(myRepo.getInfo())
+
+const custom_repo = new Repository('AngelXX', 'Luis', 'Python', 500)
+
+console.log(custom_repo.getInfo())
+
