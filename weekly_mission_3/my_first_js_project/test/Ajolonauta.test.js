103251dc791594a2234760f9a7bcd2f72b16055a
LuisAngelFnz <lfernandez@gmail.com>
2022-04-23 16:20:28 -0500

    Agregando carpeta de pruebas y archivo Ajolonauta.test.js

---
+++ weekly_mission_3/my_first_js_project/test/Ajolonauta.test.js
@@ -0,0 +1,9 @@
+const Ajolonauta = require('../app/Ajolonauta');
+
+describe('Pruebas de unidad de clase Ajolonauta', () => {
+    test('Caso de prueba 1: Creación de objeto', () => {
+        const woopa = new Ajolonauta('Woopa');
+
+        expect(woopa.name).toBe('Woopa');
+    });
+}) 
