69b76a099ddfacecd5346f76da892d6be0575a01
LuisAngelFnz <lfernandez@gmail.com>
2022-04-22 19:33:14 -0500

    2 Agregando codigo de ejercicio de operador sort

---
+++ weekly_mission_2/16_operadores.js
@@ -0,0 +1,26 @@
+// Ejemplo 16: Ordenando una lista de objetos
+const users = [
+  { name: 'A', age: 150 },
+  { name: 'B', age: 50 },
+  { name: 'C', age: 100 },
+  { name: 'D', age: 22 },
+]
+
+users.sort((a, b) => { // podemos invocar una función
+  
+  console.log('a');
+  console.log(a);
+  console.log('b');
+  console.log(b);
+
+  console.log('################################');
+
+  if (a.age < b.age) return -1
+  
+  if (a.age > b.age) return 1
+  
+  return 0
+})
+
+console.log("Ejemplo 16: Ordenando una lista de objetos por la edad")
+console.log(users) // sorted ascending
