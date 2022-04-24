06b0fad883b1219066475bb2fa8bbf07489eda72
LuisAngelFnz <lfernandez@gmail.com>
2022-04-24 11:43:37 -0500

    Creando script UserService.test.js para pruebas unitarias de la clase UserService

---
+++ weekly_mission_3/Twitter/test/models/UserService.test.js
@@ -0,0 +1,15 @@
+
+describe('Test Suit for class UserService', () => {
+    
+    const UserService = require('../../UserService');
+
+    test('Case 1) Create object from UserService', () => {
+        const obj_user_service = new UserService.create(
+            id=300,
+            username='Kinser',
+            name='Luis Angel',
+        )
+        
+
+    });
+});
