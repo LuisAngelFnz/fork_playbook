2b1f3f343a43b246a6e3701e862dadbdf1866d05
LuisAngelFnz <lfernandez@gmail.com>
2022-04-23 23:01:55 -0500

    Agregando prueba unitaria para metodos getters

--- weekly_mission_3/Twitter/test/models/User.test.js
+++ weekly_mission_3/Twitter/test/models/User.test.js
@@ -8,17 +8,28 @@ describe('Test Suit for class User', () => {
             username='Kinser',
             name='Luis Angel',
             bio='Bio',
-            dateCreated='dateCreated',
-            lastUpdated='lastUpdated'
         );
 
         expect(obj_user.id).toBe(300);
         expect(obj_user.username).toBe('Kinser');
         expect(obj_user.name).toBe('Luis Angel');
         expect(obj_user.bio).toBe('Bio');
-        expect(obj_user.dateCreated).toBe('dateCreated');
-        expect(obj_user.lastUpdated).toBe('lastUpdated');
+        expect(obj_user.dateCreated).not.toBeUndefined();
+        expect(obj_user.lastUpdated).not.toBeUndefined();
+    });
 
-    })
+    test('Case 2) Add getters methods', () => {
+        const obj_user = new User(
+            id=22,
+            username='user01',
+            name='Angel',
+            bio='Other bio'
+        );
+
+        expect(obj_user.getUsername).not.toBeUndefined();
+        expect(obj_user.getBio).not.toBeUndefined();
+        expect(obj_user.getDateCreated).not.toBeUndefined();
+        expect(obj_user.getLastUpdated).not.toBeUndefined();
+    });
 
 });
