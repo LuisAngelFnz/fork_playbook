709437e0673474ed0b41ae700a34721d4f2da41d
LuisAngelFnz <lfernandez@gmail.com>
2022-04-23 23:04:42 -0500

    Agregando codigo para solvertar prueba unitaria 2 de User.test.js

--- weekly_mission_3/Twitter/app/models/User.js
+++ weekly_mission_3/Twitter/app/models/User.js
@@ -1,11 +1,25 @@
 class User{
-    constructor(id, username, name, bio, dateCreated, lastUpdated){
+    constructor(id, username, name, bio){
         this.id          = id;
         this.username    = username;
         this.name        = name;
         this.bio         = bio;
-        this.dateCreated = dateCreated;
-        this.lastUpdated = lastUpdated;
+        this.dateCreated = new Date();
+        this.lastUpdated = new Date();
+    }
+    get getUsername(){
+        return this.username;
+    }
+
+    get getBio(){
+        return this.bio;
+    }
+
+    get getDateCreated(){
+        return this.dateCreated;
+    }
+    get getLastUpdated(){
+        return this.lastUpdated;
     }
 }
 
