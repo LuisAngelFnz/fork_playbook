116e30a08e6cfccf92c338cfc9ac6ed32006605f
LuisAngelFnz <lfernandez@gmail.com>
2022-04-23 22:26:22 -0500

    Agregando primera prueba unitaria para clase User.js

---
+++ weekly_mission_3/Twitter/test/models/User.test.js
@@ -0,0 +1,23 @@
+
+describe('Test Suit for class User', () => {
+    test('Case 1) Create object from class User',() => {
+
+        const obj_user = new User(
+            id=300,
+            username='Kinser',
+            name='Luis Angel',
+            bio='Bio',
+            dateCreated='dateCreated',
+            lastUpdated='lastUpdated'
+        );
+
+        expect(obj_user.id).toBe(300);
+        expect(obj_user.username).toBe('Kinser');
+        expect(obj_user.name).toBe('Luis Angel');
+        expect(obj_user.bio).toBe('Bio');
+        expect(obj_user.dateCreated).toBe('dateCreated');
+        expect(obj_user.lastUpdated).toBe('lastUpdated');
+
+    })
+
+});
