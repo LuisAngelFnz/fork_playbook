ffe6823396fdde83d412d7f49b1e07459a3b3287
LuisAngelFnz <lfernandez@gmail.com>
2022-04-24 15:40:20 -0500

    Agregando codigo en metodo createUser para solventar prueba 2

--- weekly_mission_3/Twitter/app/views/UserView.js
+++ weekly_mission_3/Twitter/app/views/UserView.js
@@ -2,6 +2,17 @@ class UserView{
     
     static createUser(payload){
         if (payload === null) return {'error':'payload not existe'}
+        
+        const elements = ['id','username','name'];
+        
+        while (elements){
+            let key = elements.pop();
+            if (! payload.hasOwnProperty(key) | payload[key] == null){
+                return {'error':'necesitan tener un payload válido'}
+            }
+        }
+
+
     }
 }
 
